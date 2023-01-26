3b77744135b8502fdf2fd93d0c47b09b16350059
xamelea keeko <keekoxamelea@gmail.com>
2023-01-26 18:58:20 +0300

    minor

---
+++ anilibriaAPI3/config.py
@@ -0,0 +1,6 @@
+# this is config file
+url = 'https://api.anilibria.tv'  # Anilibria API URL
+auth_url = 'https://sdsds.anilibria.icu/public/login.php'  # Anilibria auth page URL. If you from Russia use mirror
+
+password = ''
+mail = ''
